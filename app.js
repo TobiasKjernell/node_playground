165dc63907c54f9e7e658f2eaa4ab732ff765b73
DenVitaKaninen <tobiaskjernell@gmail.com>
2025-12-28 12:26:54 +0100

    added middlware method chaining, added some env variables and switched

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
-
 //Middlewares
-app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan('dev'))
+
 app.use(express.json())
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -20,4 +22,3 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
 module.exports = app;             
-        
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,26 @@
 const fs = require('fs');
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
 
+exports.checkID = (req, res, next, val) => {
+    if (req.params.id * 1 >= tours.length) {
+        return res.status(404).send({
+            status: 'failed',
+            message: 'invalid id'
+        })
+    }
+    next();
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'missing name or price'
+        })
+    }
+    next();
+}
+
 exports.getAllTours = (req, res) => {
     res.status(200).send({
         status: 'success',
@@ -16,12 +36,6 @@ exports.getSingleTour = (req, res) => {
     const id = req.params.id * 1;
     const tour = tours.find(item => item.id === id)
 
-    if (!tour) {
-        res.status(404).send({
-            status: 'failed',
-            message: 'invalid id'
-        })
-    }
     res.status(200).send({
         status: 'success',
         data: {
@@ -46,13 +60,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-    console.log(req.params.id);
-    if (req.params.id * 1 >= tours.length) {
-        res.status(404).send({
-            status: 'failed',
-            message: 'invalid id'
-        })
-    }
     res.status(200).json({
         status: 'success',
         data: {
@@ -62,13 +69,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-    console.log(req.params.id);
-    if (req.params.id * 1 >= tours.length) {
-        res.status(404).send({
-            status: 'failed',
-            message: 'invalid id'
-        })
-    }
     res.status(204).json({
         status: 'success',
         data: null
